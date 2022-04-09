5a6af75231a5204cea4029999371bad3106d1600
Ahmed <ahmed.rafed99@gmail.com>
2022-04-10 01:44:47 +0200

    implemented shift() and move() methods, few adjustments made on the coordinates. updated the method updateCell(), takes int Value as argument instead of Cell cell.

--- src/modele/Cell.java
+++ src/modele/Cell.java
@@ -12,12 +12,14 @@ public class Cell {
         this.game = null;
         this.value = value;
     }
-    public void setGame(Game game) {
-        this.game = game;
-    }
+
     public Cell(){
         this.value = NULL;
         this.game = null;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
     }
 
     public int getValue() {
@@ -25,20 +27,105 @@ public class Cell {
     }
 
     public void shift(Direction direction){
-        if (direction == Direction.up){
+        switch (direction){
+            case up :
+                if(getCoord().x != 0) {
+                    Cell cellUp = this.getNext(Direction.up);
+                    if (cellUp != null && cellUp.getValue() != this.getValue()) {
+                        game.updateCell(this.getValue(), new Point(cellUp.getCoord().x+1, cellUp.getCoord().y));
+                        this.emptyCell();
+                    }
 
+                    if(cellUp != null && cellUp.getValue() == this.getValue()){
+                        this.merge(cellUp);
+                    }
+
+                    if(cellUp == null){
+                        game.updateCell(this.getValue(), new Point(0, getCoord().y));
+                        this.emptyCell();
+                    }
+
+                }
+
+                break;
+
+            case down :
+                if(getCoord().x != this.game.getSize()-1) {
+                    Cell cellDown = this.getNext(Direction.down);
+                    if (cellDown != null && cellDown.getValue() != this.getValue()) {
+                        game.updateCell(this.getValue(), new Point(cellDown.getCoord().x-1, cellDown.getCoord().y));
+                        this.emptyCell();
+                    }
+
+                    if(cellDown != null && cellDown.getValue() == this.getValue()){
+                        this.merge(cellDown);
+                    }
+
+                    if(cellDown == null){
+                        game.updateCell(this.getValue(), new Point(game.getSize()-1, getCoord().y));
+                        this.emptyCell();
+                    }
+                }
+
+                break;
+
+            case right :
+                if(getCoord().y != this.game.getSize()) {
+                    Cell cellRight = this.getNext(Direction.right);
+                    if (cellRight != null && cellRight.getValue() != this.getValue()) {
+                        game.updateCell(this.getValue(), new Point(cellRight.getCoord().x, cellRight.getCoord().y-1));
+                        this.emptyCell();
+                    }
+
+                    if(cellRight != null && cellRight.getValue() == this.getValue()){
+                        this.merge(cellRight);
+                    }
+
+                    if(cellRight == null){
+                        game.updateCell(this.getValue(), new Point(getCoord().x, game.getSize()-1));
+                        this.emptyCell();
+                    }
+                }
+
+                break;
+
+            case left :
+                if(getCoord().y != 0) {
+                    Cell cellLeft = this.getNext(Direction.left);
+                    if (cellLeft != null && cellLeft.getValue() != this.getValue()) {
+                        game.updateCell(this.getValue(), new Point(cellLeft.getCoord().x, cellLeft.getCoord().y+1));
+                        this.emptyCell();
+                    }
+
+                    if(cellLeft != null && cellLeft.getValue() == this.getValue()){
+                        this.merge(cellLeft);
+                    }
+
+                    if(cellLeft == null){
+                        game.updateCell(this.getValue(), new Point(getCoord().x, 0));
+                        this.emptyCell();
+                    }
+                }
+
+                break;
         }
+    }
+
+    public void merge(Cell cell){
+        game.updateCell(cell.getValue() * 2, new Point(cell.getCoord().x, cell.getCoord().y));
+        game.updateCell(NULL, new Point(this.getCoord().x, this.getCoord().y));
+
     }
 
     public Cell getNext(Direction direction){
         switch (direction) {
             case up:
-                if (getCoord().y == 0) {
+                if (getCoord().x == 0) {
                     return null;
                 }
                 else {
-                    for (int y=getCoord().y; y>0; y--){
-                        Cell nextUp = game.getCell(getCoord().x, y-1);
+                    for (int x=getCoord().x; x>0; x--){
+                        Cell nextUp = game.getCell(x-1, getCoord().y);
                         if(nextUp.getValue() != NULL) {
                             return nextUp;
                         }
@@ -48,12 +135,12 @@ public class Cell {
                 }
 
             case down:
-                if (getCoord().y == game.getSize()-1) {
+                if (getCoord().x == game.getSize()-1) {
                     return null;
                 }
                 else {
-                    for (int y=getCoord().y; y<game.getSize(); y++){
-                        Cell nextDown = game.getCell(getCoord().x, y+1);
+                    for (int x=getCoord().x; x<game.getSize()-1; x++){
+                        Cell nextDown = game.getCell(x+1, getCoord().y);
                         if(nextDown.getValue() != NULL) {
                             return nextDown;
                         }
@@ -63,12 +150,12 @@ public class Cell {
                 }
 
             case left:
-                if (getCoord().x == 0) {
+                if (getCoord().y == 0) {
                     return null;
                 }
                 else {
-                    for (int x=getCoord().x; x>0; x--){
-                        Cell nextLeft = game.getCell(x-1, getCoord().y);
+                    for (int y=getCoord().y; y>0; y--){
+                        Cell nextLeft = game.getCell(getCoord().x, y-1);
                         if(nextLeft.getValue() != NULL) {
                             return nextLeft;
                         }
@@ -78,12 +165,12 @@ public class Cell {
                 }
 
             case right:
-                if (getCoord().x == game.getSize()-1) {
+                if (getCoord().y == game.getSize()-1) {
                     return null;
                 }
                 else {
-                    for (int x=getCoord().x; x<game.getSize(); x++){
-                        Cell nextRight = game.getCell(x+1, getCoord().y);
+                    for (int y = getCoord().y; y<game.getSize()-1; y++){
+                        Cell nextRight = game.getCell(getCoord().x, y+1);
                         if(nextRight.getValue() != NULL) {
                             return nextRight;
                         }
@@ -95,8 +182,27 @@ public class Cell {
         return null;
     }
 
-    public Point getCoord() {
-        return game.getCells().get(this);
+    public void setValue(int value) {
+        this.value = value;
     }
 
+    public Point getCoord() {
+        return this.game.getCells().get(this);
+    }
+
+    public void emptyCell() {
+        setValue(NULL);
+    }
+
+    @Override
+    public String toString() {
+        if (value != 0) {
+            return "{" +
+                    "" + value +
+                    "}" ;
+        } else {
+            return "{ }";
+        }
+
+    }
 }
--- src/modele/Game.java
+++ src/modele/Game.java
@@ -20,8 +20,9 @@ public class Game extends Observable {
 
         for (int i = 0; i<size; i++){
             for (int j = 0; j<size; j++){
-                this.cells.put(tabCells[i][j], new Point(i, j));
+
                 tabCells[i][j] = new Cell();
+                this.cells.put(tabCells[i][j], new Point(j, i));
             }
         }
 
@@ -32,12 +33,15 @@ public class Game extends Observable {
         }
 
 
-        rnd();
+        //rnd();
     }
 
-    public void updateCell(Cell cell, Point point){
-        tabCells[point.x][point.y] = cell;
-        cells.put(cell, point);
+    public void updateCell(int value, Point point){
+        if (point.x >= getSize() | point.y >= getSize() | point.x < 0 | point.y < 0 ) {
+            throw new IllegalArgumentException("Point must have coordinates inside the game board");
+        }
+        getCell(point.x, point.y).setValue(value);
+        cells.put(getCell(point.x, point.y), point);
     }
 
     public int getSize() {
@@ -49,12 +53,60 @@ public class Game extends Observable {
     }
 
     public void move(Direction direction){
+        switch (direction) {
+            case up:
+                for (int y = 0; y<getSize(); y++){
+                    for (int x = 0; x<getSize(); x++){
+                        Cell cell = getCell(x, y);
+                        if(cell.getValue() != NULL) {
+                            cell.shift(Direction.up);
+                        }
 
+                    }
+                }
+
+                break;
+
+            case down:
+                for (int y = 0; y<getSize(); y++){
+                    for (int x = getSize()-1; x>=0; x--){
+                        Cell cell = getCell(x, y);
+                        if(cell.getValue() != NULL) {
+                            cell.shift(Direction.down);
+                        }
+                    }
+                }
+
+                break;
+
+            case right:
+                for (int x = 0; x<getSize(); x++){
+                    for (int y = getSize()-1; y>= 0; y--){
+                        Cell cell = getCell(x, y);
+                        if (cell.getValue() != NULL) {
+                            cell.shift(Direction.right);
+                        }
+
+                    }
+                }
+
+                break;
+
+            case left:
+                for (int x = 0; x<getSize(); x++){
+                    for (int y = 0; y<getSize(); y++){
+                        Cell cell = getCell(x, y);
+                        if (cell.getValue() != NULL) {
+                            cell.shift(Direction.left);
+                        }
+                    }
+                }
+
+                break;
+        }
     }
 
-    public void merge(){
 
-    }
 
     public void rnd() {
         new Thread() { // permet de libérer le processus graphique ou de la console
@@ -73,10 +125,10 @@ public class Game extends Observable {
 
                         switch (r) {
                             case 0:
-                                updateCell(new Cell(2), new Point(x, y));
+                                updateCell(2, new Point(x, y));
                                 break;
                             case 1:
-                                updateCell(new Cell(4), new Point(x, y));
+                                updateCell(4, new Point(x, y));
                                 break;
                         }
             }
@@ -90,8 +142,27 @@ public class Game extends Observable {
 
     }
 
+    public String toString() {
+        String boardTitle = "Board" + this.getClass().getName() + "\n"
+                + "\t" + "(" + getSize() + "," + getSize() + ")" + "\n";
+
+        StringBuilder stringBuilder = new StringBuilder(boardTitle);
+        for (int i=0; i<getSize(); i++) {
+            stringBuilder.append("  | ");
+            for (int j=0; j<getSize(); j++){
+                stringBuilder.append(tabCells[i][j] + " ");
+            }
+            stringBuilder.append("|" + "\n");
+        }
+        return stringBuilder.toString();
+    }
+
     public HashMap<Cell, Point> getCells(){
         return this.cells;
+    }
+
+    public Cell[][] getTabCells(){
+        return tabCells;
     }
 
 }
